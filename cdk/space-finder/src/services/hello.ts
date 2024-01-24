ae6088fc32f42d408c32d19fed891ed72f8d0035
andqk <doquockhanhan@gmail.com>
2024-01-24 23:23:45 +0700

    feat: aws sdk with lambda ts function

--- cdk/space-finder/src/services/hello.ts
+++ cdk/space-finder/src/services/hello.ts
@@ -4,14 +4,22 @@ import {
   Context,
 } from "aws-lambda";
 import { v4 } from "uuid";
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+  const command = new ListBucketsCommand({});
+  const listBucketResult = (await s3Client.send(command)).Buckets;
+
   const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify(
       `Hello from Lambda! I will read from ${
         process.env.TABLE_NAME
-      }! A random ID: ${v4()}`
+      }! A random ID: ${v4()}! Hear are your buckets: ${JSON.stringify(
+        listBucketResult
+      )}`
     ),
   };
   console.log(event);
